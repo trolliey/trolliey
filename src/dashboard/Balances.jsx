d0d44831e0d3e0ec40352c4334a0dcde5955846d
tatendabakozw <tatendabakozw@gmail.com>
2021-11-24 00:16:21 +0200

    feat: create stuff

--- src/dashboard/Balances.jsx
+++ src/dashboard/Balances.jsx
@@ -4,7 +4,31 @@ import DashboardLayout from '../layouts/DashboardLayout'
 function Balances() {
     return (
         <DashboardLayout>
-            balances
+            <div className="flex flex-col flex-1 md:m-4 m-2 bg-white rounded p-4 ">
+                <p className="text-gray-700 font-semibold text-center">A history of all my withdrawal and deposit history</p>
+                <div className="grid grid-cols-3 bg-blue-100 capitalize rounded p-2 mt-4">
+                    <div className="col-span-1 text-center">
+                        name
+                    </div>
+                    <div className="col-span-1 text-center">
+                        date
+                    </div>
+                    <div className="col-span-1 text-center">
+                        status
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 capitalize rounded p-2 border-b border-gray-200 text-sm text-gray-500">
+                    <div className="col-span-1 text-center">
+                        rent
+                    </div>
+                    <div className="col-span-1 text-center">
+                        4 days ago
+                    </div>
+                    <div className="col-span-1 flex flex-col text-center">
+                        success
+                    </div>
+                </div>
+            </div>
         </DashboardLayout>
     )
 }
--- src/dashboard/Cards.jsx
+++ src/dashboard/Cards.jsx
@@ -4,7 +4,10 @@ import DashboardLayout from '../layouts/DashboardLayout'
 function Cards() {
     return (
         <DashboardLayout>
-            cards
+            <div className="flex flex-col flex-1 md:m-4 m-2 bg-white rounded p-4 ">
+                <p className="text-gray-700 font-semibold text-center capitalize">Info about how you want to withdraw and send money </p>
+               
+            </div>
         </DashboardLayout>
     )
 }
