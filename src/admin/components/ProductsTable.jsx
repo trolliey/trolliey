85dc07bd2129eb895e88b01e50647a5c8e83f7b0
tatendabakozw <tatendabakozw@gmail.com>
2021-12-01 14:43:11 +0200

    feat: created at made available

--- src/admin/components/ProductsTable.jsx
+++ src/admin/components/ProductsTable.jsx
@@ -3,12 +3,16 @@ import { Avatar } from "@chakra-ui/react"
 import ProductsDropdown from "./ProductsDropdown"
 import { SearchIcon } from "@heroicons/react/outline"
 import BlueButton from "../../components/buttons/BlueButton"
+import { useDispatch } from "react-redux"
+import { set_search_query_Action } from "../../redux/actions/searchAction"
+import moment from 'moment'
 
 export default function ProductsTable({ products }) {
     const [search_query, setSearchQuery] = useState('')
+    const dispatch = useDispatch()
 
     const search_item = () => {
-        console.log(search_query)
+        dispatch(set_search_query_Action(search_query))
     }
     return (
         <div className="flex flex-col flex-1 min-h-screen">
@@ -23,7 +27,7 @@ export default function ProductsTable({ products }) {
                 </div>
                 <BlueButton text="Search" onClick={search_item} />
             </div>
-            <div className="grid grid-cols-5 capitalize py-2 border-b border-gray-300 mb-2 bg-white p-2 rounded-t">
+            <div className="grid grid-cols-6 capitalize py-2 border-b border-gray-300 mb-2 bg-white p-2 rounded-t">
                 <div className="flex flex-row col-span-1 items-center gap-4">
                     <div className="text-gray-900 font-semibold">
                         title
@@ -36,12 +40,15 @@ export default function ProductsTable({ products }) {
                     <p className="text-gray-900 font-semibold">status</p>
                 </div>
                 <div className="flex col-span-1">
+                    <p className="text-gray-900 font-semibold">created</p>
+                </div>
+                <div className="flex col-span-1">
                     <p className="text-gray-900 font-semibold"></p>
                 </div>
             </div>
             {
                 products?.map((product, index) => (
-                    <div key={index} className="grid grid-cols-5 py-2 border-b border-gray-300 mb-2 text-sm items-center">
+                    <div key={index} className="grid grid-cols-6 py-2 border-b border-gray-300 mb-2 text-sm items-center">
                         <div className="flex flex-row col-span-1 items-center gap-4 overflow-ellipsis truncate">
                             <Avatar rounded="md" size="sm" name={product.title} src={product.pictures[0]} />
                             <div className="flex flex-col">
@@ -53,11 +60,14 @@ export default function ProductsTable({ products }) {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex col-span-2">
+                        <div className="flex col-span-2 px-2">
                             <p className="text-gray-700">{product.description}</p>
                         </div>
                         <div className="flex col-span-1">
                             <p className="text-gray-700">{product.status}</p>
+                        </div>
+                        <div className="flex col-span-1">
+                            <p className="text-gray-700">{moment(product.createdAt).fromNow()}</p>
                         </div>
                         <div className="flex col-span-1">
                             <div className="flex flex-col cursor-pointer">
