2be9a81f7ec81fba49cb0578926090d28b706d45
tatendabakozw <tatendabakozw@gmail.com>
2021-12-03 13:21:35 +0200

    feat: added review item

---
+++ src/components/review_item/ReviewItem.jsx
@@ -0,0 +1,31 @@
+import { Avatar } from '@chakra-ui/avatar'
+import React from 'react'
+import { ThumbDownIcon, ThumbUpIcon } from '@heroicons/react/outline'
+
+function ReviewItem({name, pro_pic, review, likes, dislikes}) {
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-row my-4">
+                <span className="md:mr-4 mr-2">
+                    <Avatar name={name} />
+                </span>
+                <div className="flex flex-col">
+                    <p className="text-gray-700 font-semibold">tatendaZw</p>
+                    <p className="text-gray-700 text-sm">{review}</p>
+                    <div className="flex flex-row items-center">
+                        <div className="flex flex-row items-center mt-2 mx-4 p-2 cursor-pointer rounded-full hover:bg-gray-100">
+                            <ThumbUpIcon className="text-gray-700" height={20} width={20} />
+                            <p className="text-sm font-semibold">{likes}</p>
+                        </div>
+                        <div className="flex flex-row items-center mt-2 mx-4 p-2 cursor-pointer rounded-full hover:bg-gray-100">
+                            <ThumbDownIcon className="text-gray-700" height={20} width={20} />
+                            <p className="text-sm font-semibold">{dislikes}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ReviewItem
--- src/pages/Single_Store/StoreReviews.jsx
+++ src/pages/Single_Store/StoreReviews.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import StoreLayout from '../../layouts/StoreLayout'
+import { PaperAirplaneIcon } from '@heroicons/react/solid'
+import ReviewItem from '../../components/review_item/ReviewItem'
 
 function StoreReviews() {
     return (
         <StoreLayout>
-            store reviews
+            <div className="flex flex-col p-4 rounded">
+                <div className="flex flex-row items-center rounded flex-1 mb-4">
+                    <input type="text" placeholder="Enter your review" className="p-2 bg-gray-100 flex-1 rounded" />
+                    <div className="flex pl-4">
+                        <PaperAirplaneIcon className="text-gray-700" height={20} width={20} />
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <ReviewItem
+                        name="mike"
+                        review="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero possimus, tempora doloribus, blanditiis accusamus numquam, aliquid quidem beatae nobis dolorem ullam consectetur perspiciatis molestias amet aspernatur! Praesentium ad fuga iure?"
+                        likes={20}
+                        dislikes={2}
+                        />
+                    <ReviewItem
+                        name={'tatendaZw'}
+                        review="iam a review"
+                        likes={2}
+                        dislikes={0}
+                    />
+                </div>
+            </div>
         </StoreLayout>
     )
 }
