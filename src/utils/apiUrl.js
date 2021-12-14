2465f3979c695da762f02d93a447092cd5a90213
tatendabakozw <tatendabakozw@gmail.com>
2021-12-14 05:06:01 +0200

    faet: compare added

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,2 +1,2 @@
-export const apiUrl = `http://localhost:5500/api/v1`
-// export const apiUrl = `https://trolliey.herokuapp.com/api/v1`
+// export const apiUrl = `http://localhost:5500/api/v1`
+export const apiUrl = `https://trolliey.herokuapp.com/api/v1`
