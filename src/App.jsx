4749a9aac4c9969ee60972feb81098672c36bbf7
tatendabakozw <tatendabakozw@gmail.com>
2022-01-22 11:50:21 +0200

    removed coing soom page in main branch

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,6 @@ import EditProduct from './dashboard/EditProduct/EditProduct';
 import Payment from './pages/Payment/Payment';
 import ManageCategories from './admin/ManageCategories';
 import Compare from './pages/Compare/Compare';
-import ComingSoon from './pages/coming_soon/ComingSoon';
 
 function App() {
     return (
